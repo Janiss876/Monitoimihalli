4b0d80d2baa967b6cd3f0baa72945b2617df654a
janiss876 <jani.saarimaki@hotmail.com>
2020-04-27 14:47:24 +0300

    c<csv<vs<v

---
+++ app/src/main/java/User.java
@@ -0,0 +1,9 @@
+public class User {
+
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String email;
+    private String phoneNumber;
+
+}
--- app/src/main/java/com/example/monitoimihalli/MainActivity.java
+++ app/src/main/java/com/example/monitoimihalli/MainActivity.java
@@ -7,11 +7,6 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    String a;
-    String b;
-    String c;
-    String f;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
