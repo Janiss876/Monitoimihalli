36aba3d6375ce2a8c5a0e7f926b9fe85adeb1778
janiss876 <jani.saarimaki@hotmail.com>
2020-04-27 14:51:07 +0300

    committi

--- app/src/main/java/com/example/monitoimihalli/MainActivity.java
+++ app/src/main/java/com/example/monitoimihalli/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     String c;
     String f;
     String jai;
+    String i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
