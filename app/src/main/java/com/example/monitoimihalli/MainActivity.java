88aaa0c266e15582d6a18cdf30e0d97b41a74422
janiss876 <jani.saarimaki@hotmail.com>
2020-04-27 13:57:30 +0300

    c<csv<vs<v

--- app/src/main/java/com/example/monitoimihalli/MainActivity.java
+++ app/src/main/java/com/example/monitoimihalli/MainActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+
 public class MainActivity extends AppCompatActivity {
+
+    String a;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
