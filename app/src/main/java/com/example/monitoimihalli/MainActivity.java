eb29e0cfc6d61f62f8a580d0909c02b57345088e
janiss876 <jani.saarimaki@hotmail.com>
2020-04-27 13:46:00 +0300

    Initial commit

---
+++ app/src/main/java/com/example/monitoimihalli/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.monitoimihalli;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
