180c64e2553625de2cca7d305765c688c23c15bb
janiss876 <jani.saarimaki@hotmail.com>
2020-05-04 14:45:31 +0300

    ---

--- app/src/main/java/com/example/monitoimihalli/Reservation.java
+++ app/src/main/java/com/example/monitoimihalli/Reservation.java
@@ -1,6 +1,55 @@
 package com.example.monitoimihalli;
 
+import java.util.ArrayList;
+
 public class Reservation {
 
+    private String roomNumber;
+    private String place;
+    private String date;
+    private String hours;
+    private String sport;
+    public static ArrayList<Reservation> reservations = new ArrayList<Reservation>();
+
+    public Reservation(String r, String p, String d, String h, String s) {
+        roomNumber = r;
+        place = p;
+        date = d;
+        hours = h;
+        sport = s;
+    }
+
+    public boolean reservationCheck(String dt, String hs) {
+        for (int i = 0; i < reservations.size(); i++) {
+            if (dt.equals(reservations.get(i).getDate()) && hs.equals(reservations.get(i).getHours())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String getRoomNumber() {
+        return roomNumber;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getHours() {
+        return hours;
+    }
+
+    public String getSport() {
+        return sport;
+    }
+
+    public void editReservation() {
+
+    }
 
 }
--- app/src/main/java/com/example/monitoimihalli/Room.java
+++ app/src/main/java/com/example/monitoimihalli/Room.java
@@ -5,4 +5,5 @@ import java.util.ArrayList;
 public class Room {
     int roomNumber;
     private ArrayList<String> availableSports;
+
 }
