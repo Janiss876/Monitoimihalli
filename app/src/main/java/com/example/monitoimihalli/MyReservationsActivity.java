09de41e843c7de1b63a82c338efe0ccd6fd09209
Arkko <arkko.ketolainen@student.lut.fi>
2020-05-06 15:30:33 +0300

    /

--- app/src/main/java/com/example/monitoimihalli/MyReservationsActivity.java
+++ app/src/main/java/com/example/monitoimihalli/MyReservationsActivity.java
@@ -1,8 +1,11 @@
 package com.example.monitoimihalli;
 
-import android.app.AppComponentFactory;
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -17,6 +20,9 @@ public class MyReservationsActivity extends AppCompatActivity {
     TextView myReservationsText;
     Spinner spinnermyreservation;
     Button showmy;
+    public static List<String> myReservations = new ArrayList<String>();
+    String myreservation;
+
 
 
 
@@ -27,7 +33,8 @@ public class MyReservationsActivity extends AppCompatActivity {
 
         edit = (Button) findViewById(R.id.editMyReservation);
         myReservationsText = (TextView) findViewById(R.id.myReservationsText);
-        spinnermyreservation = (Spinner) findViewById(R.id.spinnermyreservation);
+
+
         showmy = (Button) findViewById(R.id.showmy);
         showmy.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,10 +42,30 @@ public class MyReservationsActivity extends AppCompatActivity {
                 showMyreservations();
             }
         });
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openEdit(myreservation);
+            }
+        });
+        spinnermyreservation = (Spinner) findViewById(R.id.spinnermyreservation);
+        ArrayAdapter<String> adapter = new  ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, myReservations);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnermyreservation.setAdapter(adapter);spinnermyreservation.setAdapter(adapter);
+        spinnermyreservation.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                myreservation = parent.getItemAtPosition(position).toString();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
-    public void showMyreservations () {
+    @SuppressLint("SetTextI18n")
+    public void showMyreservations() {
         myReservationsText.setText("");
-        List<String> optionsList = new ArrayList<String>();
         for (Reservation r : Reservation.reservations) {
             if (r.getEmail().equals(User.activeUser.getEmail())) {
                 String dt = r.getDate();
@@ -47,13 +74,23 @@ public class MyReservationsActivity extends AppCompatActivity {
                 String sp = r.getSport();
                 String rm = r.getRoomNumber();
                 myReservationsText.append("Date: " + dt + " Time: " + h + " Place: " + pl + " Sport: " + sp + " Room: " + rm + "\n" + "\n");
-
+                String all = (dt+": "+ h + ": "+pl+ ": "+sp+ ": "+rm);
+                myReservations.add(all);
             }
 
         }
         if (myReservationsText.getText().toString().equals("")) {
             myReservationsText.setText("You have no reservations");
+
+
+
         }
+
+    }
+    public void  openEdit(String myreservation) {
+        startActivity(new Intent(MyReservationsActivity.this,ReservationEditActivity.class));
+
+
     }
 }
 
